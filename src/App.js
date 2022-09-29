ab47258c26aef27055d22a5bc653bea58e407cc8
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-29 21:17:52 +0600

    Activity Completed toast added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import QandA from './components/Home/QandA/QandA';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import Break from '../Break/Break';
 import './Cart.css'
 
@@ -17,6 +18,16 @@ const Cart = (props) => {
         localStorage.setItem("Break Time", event.target.innerText);
         setbreaktime(event.target.innerText)
     }
+
+    const notify = () => toast.success('🦄 Wow Activity Completed!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
 
     return (
         <div className="cart-container">
@@ -72,6 +83,20 @@ const Cart = (props) => {
 
                 <div className='break'>
                     <p className='text-xl font-bold'>Break Time: {breaktime}</p>
+                </div>
+
+                <div className='activity-completed'>
+                    <button onClick={notify} className="btn btn-primary font-bold text-black text-xl mt-10">Activity Completed</button>
+                    <ToastContainer
+                        position="top-center"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover></ToastContainer>
                 </div>
             </div>
         </div>
